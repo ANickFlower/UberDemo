c7e8f465cd4e0e44d344b9ea2818ca16241600d5
Nick Flores <anickflower@gmail.com>
2021-10-01 18:19:06 -0500

    still testing git ignore

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import { store } from "./store";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 export default function App() {
   const Stack = createNativeStackNavigator();
 
